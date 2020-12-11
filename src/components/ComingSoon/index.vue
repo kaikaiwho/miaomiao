17e6fb0d877c3640e74083781a9ccffe3f082528
kaikaiwho <kaikaiwhooo@gmail.com>
2020-12-11 17:02:40 +0800

    add createComponents

---
+++ src/components/ComingSoon/index.vue
@@ -0,0 +1,172 @@
+<template>
+    <div class="movie_body">
+        <ul>
+            <li>
+                <div class="pic_show">
+                    <img src="/images/1.jpg" alt="">
+                </div>
+                <div class="info_list">
+                    <h2>如果声音不记得</h2>
+                    <p><span class="grade">17746</span> 人想看</p>
+                    <p>主演: 章若楠,孙晨竣,王彦霖主演：</p>
+                    <p>2020-11-30上映</p>
+                </div>
+                <div class="btn_pre">
+                    预售
+                </div>
+            </li>
+            <li>
+                <div class="pic_show">
+                    <img src="/images/1.jpg" alt="">
+                </div>
+                <div class="info_list">
+                    <h2>如果声音不记得</h2>
+                    <p><span class="grade">17746</span> 人想看</p>
+                    <p>主演: 章若楠,孙晨竣,王彦霖主演：</p>
+                    <p>2020-11-30上映</p>
+                </div>
+                <div class="btn_pre">
+                    预售
+                </div>
+            </li>
+            <li>
+                <div class="pic_show">
+                    <img src="/images/1.jpg" alt="">
+                </div>
+                <div class="info_list">
+                    <h2>如果声音不记得</h2>
+                    <p><span class="grade">17746</span> 人想看</p>
+                    <p>主演: 章若楠,孙晨竣,王彦霖主演：</p>
+                    <p>2020-11-30上映</p>
+                </div>
+                <div class="btn_pre">
+                    预售
+                </div>
+            </li>
+            <li>
+                <div class="pic_show">
+                    <img src="/images/1.jpg" alt="">
+                </div>
+                <div class="info_list">
+                    <h2>如果声音不记得</h2>
+                    <p><span class="grade">17746</span> 人想看</p>
+                    <p>主演: 章若楠,孙晨竣,王彦霖主演：</p>
+                    <p>2020-11-30上映</p>
+                </div>
+                <div class="btn_pre">
+                    预售
+                </div>
+            </li>
+            <li>
+                <div class="pic_show">
+                    <img src="/images/1.jpg" alt="">
+                </div>
+                <div class="info_list">
+                    <h2>如果声音不记得</h2>
+                    <p><span class="grade">17746</span> 人想看</p>
+                    <p>主演: 章若楠,孙晨竣,王彦霖主演：</p>
+                    <p>2020-11-30上映</p>
+                </div>
+                <div class="btn_pre">
+                    预售
+                </div>
+            </li>
+            <li>
+                <div class="pic_show">
+                    <img src="/images/1.jpg" alt="">
+                </div>
+                <div class="info_list">
+                    <h2>如果声音不记得</h2>
+                    <p><span class="grade">17746</span> 人想看</p>
+                    <p>主演: 章若楠,孙晨竣,王彦霖主演：</p>
+                    <p>2020-11-30上映</p>
+                </div>
+                <div class="btn_pre">
+                    预售
+                </div>
+            </li>
+            <li>
+                <div class="pic_show">
+                    <img src="/images/1.jpg" alt="">
+                </div>
+                <div class="info_list">
+                    <h2>如果声音不记得</h2>
+                    <p><span class="grade">17746</span> 人想看</p>
+                    <p>主演: 章若楠,孙晨竣,王彦霖主演：</p>
+                    <p>2020-11-30上映</p>
+                </div>
+                <div class="btn_pre">
+                    预售
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'ComingSoon'
+}
+</script>
+
+<style lang="scss" scoped>
+    #content .movie_body {
+        flex: 1;
+        overflow: auto;
+        ul {
+            margin: 0 12px;
+            overflow: hidden;
+            li {
+                margin-top: 12px;
+                display: flex;
+                align-items: center;
+                border-bottom: 1px #e6e6e6 solid;
+                padding-bottom: 10px;
+            }
+        }
+        .pic_show {
+            width: 64px;
+            height: 90px;
+            img {
+                width: 100%;
+            }
+        }
+        .info_list {
+            margin-left: 10px;
+            flex: 1;
+            position: relative;
+            h2 {
+                font-size: 17px;
+                line-height: 24px;
+                width: 150px;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+            }
+            p {
+                font-size: 13px;
+                color: #666;
+                line-height: 22px;
+                width: 200px;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+            }
+            .grade {
+                font-weight: 700;
+                color: #faaf00;
+                font-size: 15px;
+            }
+        }
+        .btn_pre {
+            width: 47px;
+            height: 27px;
+            line-height: 28px;
+            text-align: center;
+            background-color:#3c9fe6;
+            color: #fff;
+            font-size: 12px;
+            border-radius: 4px;
+        }
+    }
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Header title="我的"/>
+        hello Mine
+        <TabBar />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+export default {
+    name : 'Mine',
+    components : {
+        Header,
+        TabBar
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
